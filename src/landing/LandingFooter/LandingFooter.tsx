2a197d20997e30a0281aac4a11c75ad858b31eb5
npavlin <np@letsnova.ru>
2024-06-15 23:31:18 +0300

    feat: футер

--- src/landing/LandingFooter/LandingFooter.tsx
+++ src/landing/LandingFooter/LandingFooter.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from './LandingFooter.module.scss'
 export const LandingFooter = () => {
   return (
-    <div className={styles.container} data-testid='test-LandingFooter'>
-      <span>Сделано с ♥️ командой DST</span>
+    <div className={styles.container}>
+      <span>Сделано с ❤️ командой ААА</span>
     </div>
   )
 }
